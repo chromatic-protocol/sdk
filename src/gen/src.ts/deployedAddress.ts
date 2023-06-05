8480ad0f4427fc695c2b79bbb045d970c6a12ad4
not-thing <not-thing@users.noreply.github.com>
2023-06-05 10:04:54 +0000

    [create-pull-request] automated change

---
+++ src/gen/src.ts/deployedAddress.ts
@@ -0,0 +1,15 @@
+
+interface DeployedAddress {
+  [chainName: string]: {
+    [contractName: string]: string
+    }    
+}
+
+export const deployedAddress: DeployedAddress = {
+  "anvil": {
+    "AccountFactory": "0xfaA7b3a4b5c3f54a934a2e33D34C7bC099f96CCE",
+    "ChromaticMarketFactory": "0x4Bd915C3e39cfF4eac842255965E79061c38cACD",
+    "ChromaticRouter": "0x86c64cB21f88fA9E2c46b61c35889E75f08FDce1",
+    "ChromaticVault": "0xCd9BC6cE45194398d12e27e1333D5e1d783104dD"
+  }
+} 

d1dbdf7fd992cd5e824c0978578906b6b6239fd6
daniel-fi <daniel-fi@users.noreply.github.com>
2023-06-26 06:28:04 +0000

    fix: clean script

--- src/gen/deployedAddress.ts
+++ src/gen/deployedAddress.ts
@@ -26,5 +26,12 @@ export const deployedAddress: DeployedAddress = {
     "ChromaticMarketFactory": "0x4Bd915C3e39cfF4eac842255965E79061c38cACD",
     "ChromaticRouter": "0x86c64cB21f88fA9E2c46b61c35889E75f08FDce1",
     "ChromaticVault": "0xCd9BC6cE45194398d12e27e1333D5e1d783104dD"
+  },
+  "arbitrum_goerli": {
+    "ChromaticLens": "0x9e6ce55E0C203Ba175bA7b2B9267427d06F2d1B5",
+    "ChromaticLiquidator": "0xA17f3F2a78b55731F2C56a410580724B85EF25dE",
+    "ChromaticMarketFactory": "0xF818D424693D12bBF4dbB97f6B43E2BAD6Ef2dCE",
+    "ChromaticRouter": "0x42baB312F31DAeC8214460DBa97Fc2aD61f2Ec87",
+    "ChromaticVault": "0x4e5b956646f77b9fAC121E29C0b7E2837785098c"
   }
 }

b2a5f3be706a2e02c2ce8cc3c87fa9cf918e642a
mark-onix <mark.kk@quarkonix.com>
2023-06-27 16:26:33 +0900

    add lens test (with addLiquidity)

--- src/entities/lens.spec.ts
+++ src/entities/lens.spec.ts
@@ -1,27 +1,78 @@
-import { ethers } from "ethers";
+import { BigNumber, BigNumberish, Signer, ethers } from "ethers";
 import { Client } from "../Client";
-import { getSigner } from "../utils/testHelpers";
+import { PromiseOrValue } from "../gen/common";
+import { getSigner, swapToUSDC, updatePrice } from "../utils/testHelpers";
+import { Interface } from "@ethersproject/abi";
+import { IERC20__factory } from "../gen";
 
 describe("lens sdk test", () => {
-  const client = new Client("anvil", getSigner());
+  const signer = getSigner();
+  const client = new Client("anvil", signer);
 
-  async function getDefaultMarket() {
+  async function getContracts() {
     const tokens = await client.marketFactory().registeredSettlementTokens();
     const markets = await client.marketFactory().getMarkets(tokens[0].address);
-    // TODO throw if market is empty
-    return markets[0].address;
+    if (markets.length == 0) {
+      throw new Error(`market is not registered (token : ${tokens[0].address})`);
+    }
+
+    return { market: markets[0].address, token: tokens[0].address, router: client.router() };
   }
 
-  test("ownedLiquidityBins view", async () => {
-    const bins = await client
-      .lens()
-      .ownedLiquidityBins(await getDefaultMarket(), "0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC");
-    console.log(bins);
-  }, 10000);
+  test("ownedLiquidityBins", async () => {
+    const { market, token } = await getContracts();
+    const beforeBins = await client.lens().ownedLiquidityBins(market, await signer.getAddress());
+
+    const amount = await swapToUSDC({
+      amount: ethers.utils.parseEther("10"),
+      signer: signer,
+      weth9: "0xe39Ab88f8A4777030A534146A9Ca3B52bd5D43A3",
+      swapRouter: "0xF1596041557707B1bC0b3ffB34346c1D9Ce94E86",
+      usdc: token,
+      // usdc: "0x8FB1E3fC51F3b789dED7557E680551d93Ea9d892",
+      fee: 3000,
+    });
+
+    const balance = await IERC20__factory.connect(token, signer).balanceOf(
+      await signer.getAddress()
+    );
+    console.log(balance);
+
+    const receipt = await client.router().addLiquidities(market, [{ feeRate: 100, amount: balance.div(2) }]);
+
+    const lpReceiptIds = await client
+      .routerContract()
+      ["getLpReceiptIds(address,address)"](market, await signer.getAddress());
+
+    await updatePrice({ market, signer, price: 1000 });
+    await client.router().claimLiquidites(market, lpReceiptIds);
+
+    const afterBins = await client.lens().ownedLiquidityBins(market, await signer.getAddress());
+    console.log("beforeBins", beforeBins);
+    console.log("afterBins", afterBins);
+    if (beforeBins.length == 0) {
+      expect(beforeBins.length + 1).toEqual(afterBins.length);
+    } else {
+      const targetBeforeBin = beforeBins.find((b) => b.tradingFeeRate == 100);
+      const targetAbterBin = afterBins.find((b) => b.tradingFeeRate == 100);
+      const beforeClbBalance =
+        targetBeforeBin === undefined ? BigNumber.from(0) : targetBeforeBin.clbBalance;
+      expect(beforeClbBalance.lt(targetAbterBin.clbBalance)).toEqual(true);
+    }
+    
+  }, 15000);
 
   test("liquidityBins", async () => {
-    const bins = await client.lens().liquidityBins(await getDefaultMarket());
-    console.log(bins);
+    const { market } = await getContracts();
+    const bins = await client.lens().liquidityBins(market);
     expect(bins.length).toEqual(72);
+    // console.log("liquidityBins", bins);
+  }, 10000);
+
+  test("claimableLiquidities", async () => {
+    const { market } = await getContracts();
+    const bins = await client.lens().liquidityBins(market);
+    expect(bins.length).toEqual(72);
+    // console.log("liquidityBins", bins);
   }, 10000);
 });
--- src/utils/testHelpers.ts
+++ src/utils/testHelpers.ts
@@ -1,4 +1,5 @@
-import { ethers } from "ethers";
+import { BigNumber, Signer, ethers } from "ethers";
+import { ChromaticMarket__factory, IERC20__factory } from "../gen";
 
 export const MNEMONIC_JUNK = "test test test test test test test test test test test junk";
 
@@ -6,6 +7,27 @@ export interface GetSignerParam {
   mnemonic?: string;
   selectedAccount?: number;
   privateKey?: string;
+}
+
+export interface WrapEthParam {
+  signer: Signer;
+  weth9: string;
+  amount: BigNumber;
+}
+
+export interface SwapToUSDCParam {
+  signer: Signer;
+  weth9: string;
+  usdc: string;
+  swapRouter: string;
+  fee: number;
+  amount: BigNumber;
+}
+
+export interface UpdatePriceParam {
+  signer: Signer;
+  market: string;
+  price: number;
 }
 
 export function getSigner(param?: GetSignerParam): ethers.Signer {
@@ -17,10 +39,161 @@ export function getSigner(param?: GetSignerParam): ethers.Signer {
 
   const account = ethers.utils.HDNode.fromMnemonic(
     param?.mnemonic === undefined ? MNEMONIC_JUNK : param!.mnemonic!
-  ).derivePath(`m/44'/60'/0'/0/${param?.selectedAccount === undefined ? 0 : param!.selectedAccount!}`);
+  ).derivePath(
+    `m/44'/60'/0'/0/${param?.selectedAccount === undefined ? 0 : param!.selectedAccount!}`
+  );
   return new ethers.Wallet(account, provider);
 }
 
 export function getDefaultProvider(): ethers.providers.JsonRpcProvider {
   return new ethers.providers.JsonRpcProvider(); // "http://localhost:8545"; // default value
 }
+
+export async function wrapEth(param: WrapEthParam) {
+  const warpTx = await param.signer.sendTransaction({
+    to: param.weth9,
+    data: ethers.utils.id("deposit()").substring(0, 10),
+    value: param.amount,
+    gasPrice: await param.signer.getGasPrice(),
+  });
+  await warpTx.wait();
+}
+
+export async function swapToUSDC(param: SwapToUSDCParam): Promise<BigNumber> {
+  const recipient = await param.signer.getAddress();
+
+  const WETH9 = IERC20__factory.connect(param.weth9, param.signer);
+  if ((await WETH9.balanceOf(recipient)).lt(param.amount)) {
+    await wrapEth({ signer: param.signer, amount: param.amount, weth9: param.weth9 });
+  }
+
+  if ((await WETH9.allowance(recipient, param.swapRouter)).lt(param.amount)) {
+    const approveTx = await IERC20__factory.connect(param.weth9, param.signer).approve(
+      param.swapRouter,
+      ethers.constants.MaxUint256
+    );
+    await approveTx.wait();
+  }
+
+  const routerContract = new ethers.Contract(
+    param.swapRouter,
+    [
+      {
+        inputs: [
+          {
+            components: [
+              {
+                internalType: "address",
+                name: "tokenIn",
+                type: "address",
+              },
+              {
+                internalType: "address",
+                name: "tokenOut",
+                type: "address",
+              },
+              {
+                internalType: "uint24",
+                name: "fee",
+                type: "uint24",
+              },
+              {
+                internalType: "address",
+                name: "recipient",
+                type: "address",
+              },
+              {
+                internalType: "uint256",
+                name: "deadline",
+                type: "uint256",
+              },
+              {
+                internalType: "uint256",
+                name: "amountIn",
+                type: "uint256",
+              },
+              {
+                internalType: "uint256",
+                name: "amountOutMinimum",
+                type: "uint256",
+              },
+              {
+                internalType: "uint160",
+                name: "sqrtPriceLimitX96",
+                type: "uint160",
+              },
+            ],
+            internalType: "struct ISwapRouter.ExactInputSingleParams",
+            name: "params",
+            type: "tuple",
+          },
+        ],
+        name: "exactInputSingle",
+        outputs: [
+          {
+            internalType: "uint256",
+            name: "amountOut",
+            type: "uint256",
+          },
+        ],
+        stateMutability: "payable",
+        type: "function",
+      },
+    ],
+    param.signer
+  );
+
+  const swapTx = await param.signer.sendTransaction({
+    to: param.swapRouter,
+    data: routerContract.interface.encodeFunctionData("exactInputSingle", [
+      {
+        tokenIn: param.weth9,
+        tokenOut: param.usdc,
+        fee: param.fee,
+        recipient: recipient,
+        deadline: ethers.constants.MaxUint256,
+        amountIn: param.amount,
+        amountOutMinimum: 0,
+        sqrtPriceLimitX96: 0,
+      },
+    ]),
+  });
+
+  const receipt = await swapTx.wait();
+  const usdcEvent = receipt.logs.find((log) => log.address == param.usdc);
+  return BigNumber.from(usdcEvent.data);
+}
+
+export async function updatePrice(param: UpdatePriceParam) {
+  const market = ChromaticMarket__factory.connect(param.market, param.signer);
+  const oracleProviderAddress = await market.oracleProvider();
+  const oracleProvider = new ethers.Contract(
+    oracleProviderAddress,
+    [
+      {
+        inputs: [
+          {
+            internalType: "Fixed18",
+            name: "price",
+            type: "int256",
+          },
+        ],
+        name: "increaseVersion",
+        outputs: [],
+        stateMutability: "nonpayable",
+        type: "function",
+      },
+    ],
+    param.signer
+  );
+
+  const tx = await param.signer.sendTransaction({
+    to: oracleProviderAddress,
+    data: oracleProvider.interface.encodeFunctionData("increaseVersion", [
+      BigNumber.from(param.price.toString()).mul(10 ** 8),
+    ]),
+  });
+
+  await tx.wait();
+}
+

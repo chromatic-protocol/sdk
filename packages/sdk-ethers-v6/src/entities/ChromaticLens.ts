45e9ac9cb1411d8c10c86564cdda6fc4f7944770
kayden-fi <kayden.choi@quarkonix.com>
2023-08-02 17:29:23 +0900

    fix: add `pendingLiquidityResult` interface

--- packages/sdk-ethers-v6/src/entities/ChromaticLens.ts
+++ packages/sdk-ethers-v6/src/entities/ChromaticLens.ts
@@ -23,6 +23,20 @@ export interface LiquidityBinResult {
 }
 
 /**
+ * Represents the result of a pending liquidity.
+ */
+export interface PendingLiquidityResult {
+  /** The oracle version for pending liquidity */
+  oracleVersion: bigint;
+  /** The amount of pending settlement token for minting */
+  mintingTokenAmountRequested: bigint;
+  /** The amount of pending CLB tokens requested for burning */
+  burningCLBTokenAmountRequested: bigint;
+  /** The trading fee rate for the liquidity bin*/
+  tradingFeeRate: number;
+}
+
+/**
  * Represents the result of an owned liquidity bin.
  */
 export interface OwnedLiquidityBinResult {
@@ -241,9 +255,12 @@ export class ChromaticLens {
    * @param tradingFeeRates An array of tradingFeeRate.
    * @returns A promise that resolves to an array of PendingLiquidity.
    */
-  async pendingLiquidityBatch(marketAddress: string, tradingFeeRates: number[]) {
+  async pendingLiquidityBatch(
+    marketAddress: string,
+    tradingFeeRates: number[]
+  ): Promise<Array<PendingLiquidityResult>> {
     return await handleBytesError(async () => {
-      const uniqTradingFeeRates = Array.from(new Set(tradingFeeRates))
+      const uniqTradingFeeRates = Array.from(new Set(tradingFeeRates));
       const pendingLiquidities = await this.getContract().pendingLiquidityBatch(
         marketAddress,
         uniqTradingFeeRates
@@ -251,13 +268,7 @@ export class ChromaticLens {
       return uniqTradingFeeRates.map((feeRate, index) => ({
         tradingFeeRate: feeRate,
         ...pendingLiquidities[index],
-      })) satisfies Array<
-        {
-          oracleVersion: bigint;
-          mintingTokenAmountRequested: bigint;
-          burningCLBTokenAmountRequested: bigint;
-        } & { tradingFeeRate: number }
-      >;
+      })) satisfies Array<PendingLiquidityResult>;
     }, this._client.provider);
   }
 
